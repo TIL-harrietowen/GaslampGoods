7033b190d80da424a81129724bad4ba4a2d67fce
Harriet Owen <harriet.owen@theinformationlab.com>
2025-02-28 22:22:54 +0000

    stg_supplies.sql created

---
+++ models/staging/stg_supplies.sql
@@ -0,0 +1,26 @@
+
+with
+
+source as (
+
+    select * from {{ source('gaslamp_goods', 'supplies') }}
+
+),
+
+renamed as (
+
+    select
+
+        ----------  ids
+        {{ dbt_utils.generate_surrogate_key(['supply_id', 'product_id']) }} as supply_uuid,
+        supply_id,
+        product_id,
+
+        ---------- properties
+        supply_cost
+
+    from source
+
+)
+
+select * from renamed
