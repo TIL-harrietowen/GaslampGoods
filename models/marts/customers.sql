48b09e8b2bbbd3750e6c22fd66da510fafcb6d6f
Harriet Owen <harriet.owen@theinformationlab.com>
2025-09-24 12:35:55 +0000

    customers.sql - removed all null customers who had not made purchases

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -22,14 +22,14 @@ final as (
     select
         customers.customer_id,
         customers.customer_name,
-        COALESCE(cust_ord.orders_count,0) as orders_per_customer,
-        cust_ord.first_order_date,
-        cust_ord.most_recent_order_date,
-        COALESCE(cust_ord.lifetime_value,0) as customer_lifetime_value
-    from customers
+        COALESCE(customer_orders.orders_count,0) as orders_per_customer,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        COALESCE(customer_orders.lifetime_value,0) as customer_lifetime_value
+    from customer_orders
 
-    left join customer_orders as cust_ord 
-        on customers.customer_id = cust_ord.customer_id
+    left join customers
+        on customer_orders.customer_id = customers.customer_id
 
 )
 
